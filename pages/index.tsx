0f7f3bedeb016a4cc97da76081b38106d305a54a
yschristian <yubahwesc@gmail.com>
2023-06-20 20:01:28 +0200

    nav bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
+import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-      hello next
-    </main>
+    <>
+      <Head>
+        <title>yuba-dev</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+        <Navbar />
+      </main>
+    </>
   )
 }
---
+++ public/assets/index.ts
@@ -0,0 +1,4 @@
+import logo from "./images/logo.png"
+import chris from "./images/chris.png"
+
+export {logo , chris}
