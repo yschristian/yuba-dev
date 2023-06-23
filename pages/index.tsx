a1feeb520241fca1afc16b8e39482ddebc94a04b
yschristian <yubahwesc@gmail.com>
2023-06-23 19:49:20 +0200

    pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,45 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import Navbar from '@/components/Navbar'
-
-const inter = Inter({ subsets: ['latin'] })
+import LeftSide from '@/components/LeftSide'
+import RightSide from '@/components/RightSide'
+import { motion } from "framer-motion"
+import Banner from '@/components/Banner'
+import About from '@/components/About'
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>yuba-dev</title>
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
         <Navbar />
+        <div className='w-full h-[88vh] xl:flex items-center gap-20 justify-between'>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className='hidden xl:inline-flex w-32 h-full left-0 bottom-0 '>
+            <LeftSide />
+          </motion.div>
+          <div className='h-[88vh] w-full mx-auto p-4'>
+            <Banner />
+            <About />
+            {/* <EXPERIENCE /> */}
+            {/* <PROJECTS /> */}
+            {/* <ARCHIVE /> */}
+            {/* <CONTACT /> */}
+          </div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32 h-full left-0 bottom-0">
+            <RightSide />
+          </motion.div>
+        </div>
       </main>
     </>
   )
