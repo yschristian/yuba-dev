b2e15c8d4a04e1d34b011ac09faead3152625e42
yschristian <yubahwesc@gmail.com>
2023-06-23 19:49:44 +0200

    react icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,7 @@ module.exports = {
         textGreen: "#64ffda",
         textLight: "#ccd6f6",
         textDark: "#8892b0",
-        hoverColor: "rgba(100,255,218,0.1 "
+        hoverColor: "rgba(100,255,218,0.1) "
       },
       fontFamily:{
         bodyFont:["Montserrat","sans-serif"],
