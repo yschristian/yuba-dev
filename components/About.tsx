7d2eb8678eed57c167af5136618ab539a3736e84
yschristian <yubahwesc@gmail.com>
2023-06-23 19:45:14 +0200

    about

---
+++ components/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section
+    id='about'
+    className="max-w-containerSmall mx-auto py-10 lgl:py-32 flex flex-col gap-8"
+    >
+        About
+    </section>
+  )
+}
+
+export default About
