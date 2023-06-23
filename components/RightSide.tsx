beffda2388ff1503dbbe3b9261c2a940f736e849
yschristian <yubahwesc@gmail.com>
2023-06-23 19:48:07 +0200

    right side

---
+++ components/RightSide.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const RightSide = () => {
+  return (
+    <div className='w-full h-full flex flex-col items-center justify-end gap-6 text-textLight'
+    >
+        <a href='mailto:yubahwesc@gmail.com'>
+            <p className='text-sm rotate-90 w-72 tracking-wide text-textGreen'>yubahwesc@gmail.com</p>
+        </a>
+        <span className='w-[2px] h-32 bg-textDark inline-flex'> </span>
+    </div>
+  )
+}
+
+export default RightSide
