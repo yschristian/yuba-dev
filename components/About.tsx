531ea362df450c66700e3ee98ef286d631d254e1
yschristian <yubahwesc@gmail.com>
2023-06-25 12:51:43 +0200

    about

--- components/About.tsx
+++ components/About.tsx
@@ -1,12 +1,71 @@
 import React from 'react'
+import SectionTitle from './SectionTitle'
+import { AiFillThunderbolt } from "react-icons/ai"
+import Image from 'next/image'
+import { chris } from '@/public/assets'
 
 const About = () => {
   return (
     <section
-    id='about'
-    className="max-w-containerSmall mx-auto py-10 lgl:py-32 flex flex-col gap-8"
+      id='about'
+      className="max-w-containerSmall mx-auto py-10 lgl:py-32 flex flex-col gap-8"
     >
-        About
+      <SectionTitle title="About Me" titleNo="01" />
+      <div className='flex flex-col lgl:flex-row gap-16'>
+        <div className='w-full lgl:w-2/3 text-base text-textDark font-medium flex flex-col gap-4'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ullam iste eos necessitatibus quam deleniti minima tempora fugiat numquam non beatae cupiditate inventore a iure aliquid voluptatum quisquam adipisci unde dolorem expedita,
+            distinctio perferendis? Et nisi provident blanditiis accusamus deleniti, tempora similique repudiandae, magnam aliquam explicabo sequi praesentium tempore optio sunt.
+          </p>
+          <p>Here are a few technologies i have been working with recently! </p>
+          <ul className='max-w-[450px] text-sm font-titleFont grid grid-cols-2 gap-2 mt-6'>
+            <li className="flex items-center gap-2">
+              <span className='text-textGreen'><AiFillThunderbolt /></span>Javascript(ES6+)
+            </li>
+            <li className="flex items-center gap-2">
+              <span className='text-textGreen'><AiFillThunderbolt /></span>React
+            </li>
+            <li className="flex items-center gap-2">
+              <span className='text-textGreen'><AiFillThunderbolt /></span>Typescript
+            </li>
+            <li className="flex items-center gap-2">
+              <span className='text-textGreen'><AiFillThunderbolt /></span>MongoDB
+            </li>
+            <li className="flex items-center gap-2">
+              <span className='text-textGreen'><AiFillThunderbolt /></span>PostgreSQL
+            </li>
+            <li className="flex items-center gap-2">
+              <span className='text-textGreen'><AiFillThunderbolt /></span>Next.js
+            </li>
+            <li className="flex items-center gap-2">
+              <span className='text-textGreen'><AiFillThunderbolt /></span>Node.js
+            </li>
+            <li className="flex items-center gap-2">
+              <span className='text-textGreen'><AiFillThunderbolt /></span>Express.js
+            </li>
+            <li className="flex items-center gap-2">
+              <span className='text-textGreen'><AiFillThunderbolt /></span>Tailwindcss
+            </li>
+          </ul>
+        </div>
+        <div className='w-full lgl:w-1/3 h-80 relative group'>
+          <div className='absolute w-full h-80 -left-6 -top-6 rounded-lg'>
+            <div className='w-full h-full relative z-20 flex pl-6 lgl:pl-0'>
+              <Image
+                className="rounded-lg h-full object-cover"
+                src={chris}
+                alt="profile Img"
+              />
+              <div className='hidden lgl:inline-block absolute w-full h-80 bg-textGreen/20 
+                   rounded-md top-0 left-0 group-hover:bg-transparent duration-300' ></div>
+            </div>
+          </div>
+          <div className='hidden lgl:inline-flex w-full h-80 border-2 border-textGreen 
+            rounded-md group-hover:-translate-x-2 group-hover:-translate-y-2 
+            transition-transform duration-300'></div>
+        </div>
+      </div>
     </section>
   )
 }
