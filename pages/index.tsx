7554a9b9d93a4534c137ad9ca6622e8b6e89a62b
yschristian <yubahwesc@gmail.com>
2023-06-25 12:55:08 +0200

    pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ import RightSide from '@/components/RightSide'
 import { motion } from "framer-motion"
 import Banner from '@/components/Banner'
 import About from '@/components/About'
+import Experience from '@/components/Experience'
+import Projects from '@/components/Projects'
+import Archive from '@/components/Archive'
 
 export default function Home() {
   return (
@@ -28,8 +31,9 @@ export default function Home() {
           <div className='h-[88vh] w-full mx-auto p-4'>
             <Banner />
             <About />
-            {/* <EXPERIENCE /> */}
-            {/* <PROJECTS /> */}
+            <Experience/>
+            <Projects />
+            <Archive />
             {/* <ARCHIVE /> */}
             {/* <CONTACT /> */}
           </div>
@@ -37,7 +41,7 @@ export default function Home() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 1.5 }}
-            className="hidden xl:inline-flex w-32 h-full left-0 bottom-0">
+            className="hidden xl:inline-flex w-32 h-full right-0 bottom-0">
             <RightSide />
           </motion.div>
         </div>
