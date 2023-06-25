3d3ad47487f0f9345a5d376ef92f7d5dbca8af13
yschristian <yubahwesc@gmail.com>
2023-06-25 12:56:30 +0200

    images

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,4 +1,7 @@
 import logo from "./images/logo.png"
 import chris from "./images/chris.png"
+import ecommerce from "./images/ecommerce.png"
+import tpc from "./images/tpc.png"
+import uhdss from "./images/uhdss.png"
 
-export {logo , chris}
+export {logo , chris, ecommerce ,tpc, uhdss}
