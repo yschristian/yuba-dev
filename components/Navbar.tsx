bfefa8c39c1a4977d6084185efb52faff0c3de0e
yschristian <yubahwesc@gmail.com>
2023-06-28 13:55:23 +0200

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,26 @@
 import { logo } from '@/public/assets'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { motion } from 'framer-motion'
 
 const Navbar = () => {
+    const ref = useRef<string | any>("")
+    const [showMenu , setShowMenu ] = useState(false)
+    const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        e.preventDefault()
+        const href = e.currentTarget.href
+        const targetId = href.replace(/.*\#/,"");
+        const elem = document.getElementById(targetId)
+        elem ?. scrollIntoView({
+            behavior: "smooth",
+        })
+        const links = document.querySelectorAll(".nav-link")
+        links.forEach(link => {
+            link.classList.remove("active")
+        });
+        e.currentTarget.classList.add("active")
+    }
     return (
         <div className='w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor'>
             <div className='max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify-between'>
@@ -13,12 +29,13 @@ const Navbar = () => {
                     animate={{ opacity: 1 }}
                     transition={{ duration: 0.5 }}
                 >
-                    <Image className='w-14' src={logo} alt="logo" />
+                    <Image className='w-[160px]' src={logo} alt="logo" />
                 </motion.div>
                 <div className='hidden mdl:inline-flex items-center gap-7'>
                     <ul className='flex text-[13px] gap-7 '>
                         <Link
                             href="#home"
+                            onClick={handleScroll}
                             className='flex items-center gap-1 font-medium text-textDark
                              hover:text-textGreen cursor-pointer duration-300 nav-link'
                         >
@@ -33,6 +50,7 @@ const Navbar = () => {
 
                         <Link
                             href="#about"
+                            onClick={handleScroll}
                             className='flex items-center gap-1 font-medium text-textDark
                              hover:text-textGreen cursor-pointer duration-300 nav-link'
                         >
@@ -45,6 +63,7 @@ const Navbar = () => {
                             </motion.li>
                         </Link>
                         <Link
+                            onClick={handleScroll}
                             href="#experience"
                             className='flex items-center gap-1 font-medium text-textDark
                              hover:text-textGreen cursor-pointer duration-300 nav-link'
@@ -58,6 +77,7 @@ const Navbar = () => {
                             </motion.li>
                         </Link>
                         <Link
+                            onClick={handleScroll}
                             href="#project"
                             className='flex items-center gap-1 font-medium text-textDark
                              hover:text-textGreen cursor-pointer duration-300 nav-link'
@@ -71,6 +91,7 @@ const Navbar = () => {
                             </motion.li>
                         </Link>
                         <Link
+                            onClick={handleScroll}
                             href="#contact"
                             className='flex items-center gap-1 font-medium text-textDark
                              hover:text-textGreen cursor-pointer duration-300 nav-link'
@@ -85,26 +106,34 @@ const Navbar = () => {
                         </Link>
                     </ul>
                     <a href='/assets/my_resume.pdf' target="_blank">
-                    <motion.button
-                        initial={{ y: -10, opacity: 0 }}
-                        animate={{ y: 0, opacity: 1 }}
-                        transition={{ delay: 0.5 }}
-                        className="px-4 px-2 rounded-md text-textGreen text-[13px] border border-textGreen
+                        <motion.button
+                            initial={{ y: -10, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            transition={{ delay: 0.5 }}
+                            className="px-4 py-2 rounded-md text-textGreen text-[13px] border border-textGreen
                         hover:bg-hoverColor duration-300
                         "
-                    >
-                        Resume
-                    </motion.button>
+                        >
+                            Resume
+                        </motion.button>
                     </a>
                 </div>
-                <div className='w-6 h-5 flex flex-col justify-between items-center mdl:hidden text-4xl text-textGreen cursor-pointer overflow-hidden group'>
+                <div onClick={() => setShowMenu(true)} className='w-6 h-5 flex flex-col justify-between items-center mdl:hidden text-4xl text-textGreen cursor-pointer overflow-hidden group'>
                     <span className='w-full h-[2px] bg-textGreen inline-flex transform 
                     group-hover:translate-x-2 transition-all ease-in-out duration-300'></span>
                     <span className='w-full h-[2px] bg-textGreen inline-flex transform translate-x-3
                     group-hover:translate-x-2 transition-all ease-in-out duration-300'></span>
                     <span className='w-full h-[2px] bg-textGreen inline-flex transform translate-x-1
-                    group-hover:translate-x-3 transition-all ease-in-out duration-300'></span> 
+                    group-hover:translate-x-3 transition-all ease-in-out duration-300'></span>
                 </div>
+                { showMenu && (
+                    <div ref= {(node) => (ref.current = node)}
+                    className='absolute mdl:hidden top-7 right-0 w-full h-screen bg-black bg-opacity-50 flex flex-col items-end'
+                    >
+                        <div></div>
+                    </div>
+                )
+                }
             </div>
         </div>
     )
