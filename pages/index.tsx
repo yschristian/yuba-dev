eb531d75834bee29f867a919878a2036645948e0
yschristian <yubahwesc@gmail.com>
2023-06-28 13:56:06 +0200

    pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,15 +10,17 @@ import About from '@/components/About'
 import Experience from '@/components/Experience'
 import Projects from '@/components/Projects'
 import Archive from '@/components/Archive'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>yuba-dev</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
       </Head>
-      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60 ">
         <Navbar />
         <div className='w-full h-[88vh] xl:flex items-center gap-20 justify-between'>
           <motion.div
@@ -34,8 +36,8 @@ export default function Home() {
             <Experience/>
             <Projects />
             <Archive />
-            {/* <ARCHIVE /> */}
-            {/* <CONTACT /> */}
+            <Contact />
+            <Footer />
           </div>
           <motion.div
             initial={{ opacity: 0 }}
