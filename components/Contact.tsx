cd6c180094369f94381dc363116fcc9d1cf96b8b
yschristian <yubahwesc@gmail.com>
2023-06-29 16:57:02 +0200

    navbar

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,15 +4,15 @@ const Contact = () => {
   return (
     <section 
     id="contact"
-    className='max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center '
+    className="max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center"
     >
-        <p className='font-titleFont text-lg text-textGreen font-semibold flex items-center tracking-wider'>
+        <p className="font-titleFont text-lg text-textGreen font-semibold flex items-center tracking-wider">
             04. What's Next?
         </p>
-        <h2 className='font-titleFont text-5xl font-semibold'>Get In Touch</h2>
+        <h2 className="font-titleFont text-5xl font-semibold">Get In Touch</h2>
         {/* <p className='max-w-[600px] text-center text-textDark'></p> */}
 
-        <a href='mailto:yubahwesc@gmail.com'>
+        <a href="mailto:yubahwesc@gmail.com">
             <button className = "w-40 h-14 border border-textGreen MT-6 font-titleFont text-sm text-textGreen tracking-wider rounded-md hover:bg-hoverColor duration-300 ">
                 Say Hello!!
             </button>
